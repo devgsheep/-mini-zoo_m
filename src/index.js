e89451d2e1837cf1ad926462e967f2f9e60cd7a2
devdorong <dev.dorong@gmail.com>
2025-07-22 14:26:05 +0900

    [docs] 테마컬러 수정

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Home from "./pages/Home";
+import FirstForm from "./pages/FirstForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(<FirstForm />);

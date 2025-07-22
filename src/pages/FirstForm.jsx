c9410b5f0582df66dd4fb2a1473d278ac9e2d81f
zzeondev <zzeon.dev@gmail.com>
2025-07-22 18:11:54 +0900

    [docs] 프로필 작업

--- src/pages/FirstForm.jsx
+++ src/pages/FirstForm.jsx
@@ -2,7 +2,7 @@ import React from "react";
 // window
 
 function FirstForm() {
-    //js
+  //js
 
   //jsx
   return (
@@ -10,23 +10,15 @@ function FirstForm() {
       <h1>로그인</h1>
       <div>
         <form>
-          <input
-            type="email"
-            value={userEmail}
-            placeholder="등록된 이메일을 입력하세요"
-          />
-          <input
-            type="password"
-            value={userPassword}
-            placeholder="비밀번호를 입력하세요"
-          />
+          <input type="email" placeholder="등록된 이메일을 입력하세요" />
+
+          <input type="password" placeholder="비밀번호를 입력하세요" />
           {/* 체크박스 */}
           <button type="submit">로그인</button>
         </form>
       </div>
     </div>
   );
-
 }
 
 export default FirstForm;

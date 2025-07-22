65198f54ad9d565ac919d79824b1d09ca1e04170
devdorong <dev.dorong@gmail.com>
2025-07-22 18:09:46 +0900

    [docs] first, home 초기작업완료, today 진행중

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-import Profile from "./pages/Profile";
-import Home from "./pages/Home";
-import FirstForm from "./pages/FirstForm";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);

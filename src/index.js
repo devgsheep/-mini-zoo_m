004e748ffcd338acfb2a1dbd32760973e8dc781e
zzeondev <zzeon.dev@gmail.com>
2025-07-22 14:26:00 +0900

    [docs] prifile 작업중

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Profile from "./pages/Profile";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<Profile />);

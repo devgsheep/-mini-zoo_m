0c304631cc281c799056141338af6e1af1bbc3e7
devgsheep <dev.gsheep@gmail.com>
2025-07-23 15:24:00 +0900

    [docs] 07.23

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -43,6 +43,40 @@ export const colors = {
     800: "#424242",
     900: "#292929",
   },
+  emotion: {
+    happy: {
+      base: "#ffdb65",
+      hover: "#f4c738", // 진한 밝은 노랑
+    },
+    sad: {
+      base: "#6b9dfa",
+      hover: "#4a7bd8", // 어두운 파랑
+    },
+    angry: {
+      base: "#fa6b6b",
+      hover: "#e64747", // 강한 붉은색
+    },
+    boring: {
+      base: "#8E8AF9",
+      hover: "#6f6cf0", // 더 진한 보라
+    },
+    anxious: {
+      base: "#FB9E34",
+      hover: "#e68213", // 진한 주황
+    },
+    disgust: {
+      base: "#38E25D",
+      hover: "#27c34d", // 어두운 초록
+    },
+    embarrassed: {
+      base: "#E59FDC",
+      hover: "#cc6fbc", // 붉은 핑크
+    },
+    tired: {
+      base: "#6DEBD2",
+      hover: "#45cab7", // 어두운 민트
+    },
+  },
 };
 
 export default colors;

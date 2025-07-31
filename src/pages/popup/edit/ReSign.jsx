5f57d792dea91054714ebf6ad7b68ec0c62c56de
zzeondev <zzeon.dev@gmail.com>
2025-07-31 14:26:01 +0900

    [docs] 모달 작업진행

--- src/pages/popup/edit/ReSign.jsx
+++ src/pages/popup/edit/ReSign.jsx
@@ -129,7 +129,7 @@ function ReSign({ onClose }) {
 
   return (
     // <Container>
-    //   <ReSignPopUp>
+    //    <ReSignPopUp>
     <ReSignPopUpBox>
       <ResignTopBox>
         <ResignImgBox>
